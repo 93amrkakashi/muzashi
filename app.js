32d4428c2cd8146043a360d9187921d1beb8dc93
amr kakashi <93amr.kakashi@gmail.com>
2022-09-28 23:13:49 +0200

    media queries

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const section = document.querySelector(".skills");
 const spans = document.querySelectorAll(".progress span");
+let switchs = document.querySelector(".menu");
+let link = document.querySelector(".links");
+let navBar = document.getElementById("nav");
 
+
+console.log(switchs)
+function switching(){
+    link.classList.toggle("mobile");
+    // link.classList.toggle("glass");
+
+}
+switchs.addEventListener("click",switching);
 
 
 
 window.onscroll = function () {
-    if (window.scrollY >= section.offsetTop - 250) {
+    if (window.scrollY >= section.offsetTop - 300) {
         spans.forEach((span) => {
             span.style.width = span.dataset.width;
         });
